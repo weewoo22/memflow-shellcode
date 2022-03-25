6b8c5478e47514145afc920cc79b3ea41cf02d11
weewoo22 <91314050+weewoo22@users.noreply.github.com>
2022-03-24 21:31:44 -0400

    Annoying but necessary

--- flake.nix
+++ flake.nix
@@ -6,6 +6,19 @@
     memflow.url = github:memflow/memflow-nixos;
     nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
     zig-overlay.url = github:arqv/zig-overlay;
+
+    zig-win32 = {
+      url = github:marlersoft/zigwin32/032a1b51b83b8fe64e0a97d7fe5da802065244c6;
+      flake = false;
+    };
+    zig-clap = {
+      flake = false;
+      url = github:Hejsil/zig-clap/0970eb827fe53ad7a6c6744019707190d7b9bb32;
+    };
+    zig-args = {
+      flake = false;
+      url = github:MasterQ32/zig-args/1ff417ac1f31f8dbee3a31e5973b46286d42e71d;
+    };
   };
 
   nixConfig = {
@@ -32,6 +45,14 @@
 
             src = ./.;
 
+            postUnpack = ''
+              rm -rf $sourceRoot/libs/
+              mkdir -vp $sourceRoot/libs/{zigwin32,zig-clap,zig-args}/
+              cp -a ${inputs.zig-win32}/* $sourceRoot/libs/zigwin32/
+              cp -a ${inputs.zig-clap}/* $sourceRoot/libs/zig-clap/
+              cp -a ${inputs.zig-args}/* $sourceRoot/libs/zig-args/
+              chmod a+r+w $sourceRoot/libs/*
+            '';
             buildPhase = ''
               # Set Zig global cache directory
               export XDG_CACHE_HOME="$TMPDIR/zig-cache/"
@@ -49,3 +70,4 @@
       }
     );
 }
+
